41b45492814adb40c2d0c91c2095b878244501fd
SARVPRIYE SONI <sarvpriye98@gmail.com>
2012-04-03 22:48:46 +0530

    validation

--- ThoughtXplore/ThoughtXplore/txMisc/Validation.py
+++ ThoughtXplore/ThoughtXplore/txMisc/Validation.py
@@ -9,3 +9,22 @@ class EmailValidate():
             if re.match("^.+\\@(\\[?)[a-zA-Z0-9\\-\\.]+\\.([a-zA-Z]{2,3}|[0-9]{1,3})(\\]?)$", self.email) != None:
                 return 1
         return 0
+    
+class validatestring():
+    
+    def validate_alphastring(self,st):
+        if(st.isalpha()):
+            return 1
+        return 0
+    def validate_alphanumstring(self,st):
+        if(st.isalnum()):
+            return 1
+        return 0
+    def validate_numeric(self,st):
+        if(st.isdigit()):
+            return 1
+        return 0
+    
+    
+            
+            
