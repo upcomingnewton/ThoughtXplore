45cbd112fd10697fe4d9a69a583717e9a080f7b7
SARVPRIYE SONI <sarvpriye98@gmail.com>
2012-04-03 17:43:24 +0530

    sttings for server

--- ThoughtXplore/ThoughtXplore/settings.py
+++ ThoughtXplore/ThoughtXplore/settings.py
@@ -14,8 +14,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'ThoughtXplore',                      # Or path to database file if using sqlite3.
         'USER': 'tpcuiet2013',                      # Not used with sqlite3.
-        'PASSWORD': 'Nath@3728',                  # Not used with sqlite3.
-        'HOST': '119.18.58.211',                      # Set to empty string for localhost. Not used with sqlite3.
+        'PASSWORD': 'Nath@3728.com',                  # Not used with sqlite3.
+        'HOST': '0.0.0.0',                      # Set to empty string for localhost. Not used with sqlite3.
         'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
     }
 }
